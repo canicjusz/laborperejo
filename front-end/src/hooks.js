5360d6eb63afb19ac42790283653df6195ad90b9
canicjusz <janek.bananek371@gmail.com>
2024-05-29 16:32:30 +0200

    remove annoying error that stops building process

--- front-end/src/hooks.js
+++ front-end/src/hooks.js
@@ -4,9 +4,8 @@ import { variables } from '$lib/variables';
 export async function handle({ event, resolve }) {
 	event.locals.user = await getData(variables.base + `/api/profiles/mine`, true, {
 		cookie: event.request.headers.get('cookie')
-	});
+	}).catch(() => {});
 	const response = await resolve(event);
-
 	return response;
 }
 
